4497054df4ac16b78c99a38d0986471a54dee009
Hasnain Makada <hasnainmakada@gmail.com>
2023-09-09 15:48:09 +0530

    [update] Component completed

--- home_widget_demo/lib/views/home_widget/home_widget.dart
+++ home_widget_demo/lib/views/home_widget/home_widget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_weather_bg_null_safety/utils/weather_type.dart';
 // import 'package:flutter_weather_bg_null_safety/utils/weather_type.dart';
 import 'package:home_widget_demo/models/weather.dart';
 
@@ -12,32 +13,66 @@ class HomeWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: 170,
-      height: 170,
-      decoration: BoxDecoration(
-        image: DecorationImage(
-          image: NetworkImage(
-              'https://raw.githubusercontent.com/aronvisser19/ye-said/main/android-widgets/images/ksg-widget%403x.png'),
-          fit: BoxFit.fitHeight,
+    // return Container(
+    //   width: 170,
+    //   height: 170,
+    //   decoration: BoxDecoration(
+    //     image: DecorationImage(
+    //       image: NetworkImage(
+    //         'https://raw.githubusercontent.com/aronvisser19/ye-said/main/android-widgets/images/ksg-widget%403x.png',
+    //       ),
+    //       fit: BoxFit.cover,
+    //     ),
+    //   ),
+    //   child: Column(
+    //     children: [
+    //       Container(
+    //         width: 170,
+    //         height: 170,
+    //         decoration: BoxDecoration(
+    //           image: DecorationImage(
+    //             image: NetworkImage(
+    //               'https://raw.githubusercontent.com/aronvisser19/ye-said/main/android-widgets/images/ksg-widget%403x.png',
+    //             ),
+    //             fit: BoxFit.cover,
+    //           ),
+    //         ),
+    //       ),
+    //       Text("Wrestlin' with God, I don't really want to wrestle",
+    //           style: const TextStyle(
+    //               color: Colors.white, fontWeight: FontWeight.bold),
+    //           textScaleFactor: 1.7),
+    //       // Text(weather.cityName,
+    //       //     style: const TextStyle(color: Colors.white60, fontSize: 14)),
+    //     ],
+    //   ),
+    // );
+
+    return Stack(
+      children: [
+        Positioned.fill(
+          child: Image.network(
+            'https://raw.githubusercontent.com/aronvisser19/ye-said/main/android-widgets/images/ksg-widget%403x.png',
+            fit: BoxFit.cover,
+          ),
         ),
-      ),
-      // decoration: BoxDecoration(
-      //   gradient: LinearGradient(
-      //     colors: WeatherUtil.getColor(weather.weatherType),
-      //   ),
-      // ),
-      child: Column(
-        children: [
-          Image.network(weather.image),
-          Text("Wrestlin' with God, I don't really want to wrestle",
-              style: const TextStyle(
-                  color: Colors.white, fontWeight: FontWeight.bold),
-              textScaleFactor: 1.7),
-          // Text(weather.cityName,
-          //     style: const TextStyle(color: Colors.white60, fontSize: 14)),
-        ],
-      ),
+
+        SizedBox(
+          height: 100,
+          width: 100,
+          child: Center(
+            child: Text(
+              "Wrestlin' with God, I don't really want to wrestle",
+              textAlign: TextAlign.center,
+              style: TextStyle(fontSize: 10),
+            ),
+          ),
+        )
+
+        //           style: const TextStyle(
+        //               color: Colors.white, fontWeight: FontWeight.bold),
+        //           textScaleFactor: 1.7),
+      ],
     );
   }
 }
