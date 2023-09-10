961167750440698aa17faa5c7794cf43442562f0
yung-coder <chandel.sanjeev5@gmail.com>
2023-09-10 19:57:29 +0530

    clean

--- home_widget_demo/lib/home_widget_config.dart
+++ home_widget_demo/lib/home_widget_config.dart
@@ -4,11 +4,12 @@ import 'dart:typed_data';
 import 'package:davinci/davinci.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:home_widget/home_widget.dart';
-import 'package:home_widget_demo/constants/strings.dart';
 import 'package:path_provider/path_provider.dart';
 
 class HomeWidgetConfig {
   static Future<void> update(context, Widget widget) async {
+    String iosWidget = 'home_widget_demo';
+    String androidWidget = 'CustomHomeView';
     Uint8List bytes = await DavinciCapture.offStage(widget,
         context: context,
         returnImageUint8List: true,
@@ -26,6 +27,7 @@ class HomeWidgetConfig {
   }
 
   static Future<void> initialize() async {
+    String groupId = 'group.home_widget_demo_group';
     await HomeWidget.setAppGroupId(groupId);
   }
 }
--- home_widget_demo/lib/views/home_widget/home_widget.dart
+++ home_widget_demo/lib/views/home_widget/home_widget.dart
@@ -1,62 +1,23 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_weather_bg_null_safety/utils/weather_type.dart';
-// import 'package:flutter_weather_bg_null_safety/utils/weather_type.dart';
-import 'package:home_widget_demo/models/weather.dart';
 
 class HomeWidget extends StatelessWidget {
-  final Weather weather;
+  final String imgURL;
 
   const HomeWidget({
     Key? key,
-    required this.weather,
+    required this.imgURL,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    // return Container(
-    //   width: 170,
-    //   height: 170,
-    //   decoration: BoxDecoration(
-    //     image: DecorationImage(
-    //       image: NetworkImage(
-    //         'https://raw.githubusercontent.com/aronvisser19/ye-said/main/android-widgets/images/ksg-widget%403x.png',
-    //       ),
-    //       fit: BoxFit.cover,
-    //     ),
-    //   ),
-    //   child: Column(
-    //     children: [
-    //       Container(
-    //         width: 170,
-    //         height: 170,
-    //         decoration: BoxDecoration(
-    //           image: DecorationImage(
-    //             image: NetworkImage(
-    //               'https://raw.githubusercontent.com/aronvisser19/ye-said/main/android-widgets/images/ksg-widget%403x.png',
-    //             ),
-    //             fit: BoxFit.cover,
-    //           ),
-    //         ),
-    //       ),
-    //       Text("Wrestlin' with God, I don't really want to wrestle",
-    //           style: const TextStyle(
-    //               color: Colors.white, fontWeight: FontWeight.bold),
-    //           textScaleFactor: 1.7),
-    //       // Text(weather.cityName,
-    //       //     style: const TextStyle(color: Colors.white60, fontSize: 14)),
-    //     ],
-    //   ),
-    // );
-
     return Stack(
       children: [
         Positioned.fill(
           child: Image.network(
-            'https://raw.githubusercontent.com/aronvisser19/ye-said/main/android-widgets/images/ksg-widget%403x.png',
+            imgURL,
             fit: BoxFit.cover,
           ),
         ),
-
         SizedBox(
           height: 100,
           width: 100,
@@ -68,10 +29,6 @@ class HomeWidget extends StatelessWidget {
             ),
           ),
         )
-
-        //           style: const TextStyle(
-        //               color: Colors.white, fontWeight: FontWeight.bold),
-        //           textScaleFactor: 1.7),
       ],
     );
   }
