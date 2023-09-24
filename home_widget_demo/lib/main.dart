25fa385023315160f4d422324a434289a1862bf4
Hasnain Makada <hasnainmakada@gmail.com>
2023-09-24 10:22:39 +0530

    [update]: Frontend complete, Swipe to refresh remaining

--- home_widget_demo/lib/main.dart
+++ home_widget_demo/lib/main.dart
@@ -1,6 +1,10 @@
-import 'package:flutter/material.dart';
-import 'package:home_widget_demo/views/dashboard/dashboard_screen.dart';
+import 'dart:async';
 
+import 'package:flutter/material.dart';
+import 'package:dio/dio.dart';
+import 'package:pull_to_refresh/pull_to_refresh.dart';
+
+final dio = Dio();
 void main() {
   runApp(const MyApp());
 }
@@ -11,11 +15,94 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Home Widget Demo',
       theme: ThemeData.dark(
         useMaterial3: true,
       ),
-      home: const DashboardScreen(),
+      home: const HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  RefreshController _refreshController = RefreshController();
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+
+    fetchQuotes();
+  }
+
+  var quote = '';
+
+  Future<void> fetchQuotes() async {
+    const api = 'https://bpstudios.nl/daily_ye_backend/get_quote.php';
+    final res = await dio.get(api);
+    setState(() {});
+    quote = res.data.toString();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color.fromRGBO(247, 140, 88, 1.0),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Expanded(
+              flex: 1, // Empty space at the top to push content down
+              child: Align(
+                alignment: Alignment.center,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      '${quote}',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontFamily: 'Helvetica',
+                        fontSize: 22,
+                        color: Colors.black,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                    Container(
+                      margin: EdgeInsets.only(top: 10),
+                      padding: EdgeInsets.only(left: 99.0),
+                      child: Text(
+                        ' KANYE WEST',
+                        style: TextStyle(
+                          fontSize: 14,
+                          fontWeight: FontWeight.w600,
+                          fontFamily: 'Helvetica',
+                          color: Colors.black,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.only(bottom: 50),
+              width: 75,
+              height: 50,
+              child: Image.asset('asset/parental-advisory.png'),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
